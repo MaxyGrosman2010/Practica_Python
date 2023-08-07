0794633134ac6bcf7e84913718a11101332faa6f
MaxyGrosman <maxygrosman@gmail.com>
2023-08-07 17:31:04 -0300

    First class

---
+++ Fundamentos/1ra-Hello_World.py
@@ -0,0 +1,18 @@
+#Hello World
+print('Hello World, this is Python')
+"""All
+of this
+it is a comment
+on multiple lines"""
+
+#Complex numbers
+print(1 + 1j, ' is ', type(1 + 1j))
+#List
+print([0, 1, 2 , 3], ' is ' ,type([0, 1, 2 , 3]))
+#Dictionary
+print({'Hello': 'World', 'I': 'am', 'learning': 'Python'}, ' is ', 
+      type({'Hello': 'World', 'I': 'am', 'learning': 'Python'}))
+#Tuple
+print(('Hello','world'), ' is ',type(('Hello','world')))
+#Set
+print({2, 3, 4, 5}, ' is ',type({2, 3, 4, 5}))
