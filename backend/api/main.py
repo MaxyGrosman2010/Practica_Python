6d10659462fb05a085dac578b927ec47d46f6e36
MaxyGrosman <maxygrosman@gmail.com>
2023-09-04 20:44:55 -0300

    Small progress

--- backend/api/main.py
+++ backend/api/main.py
@@ -4,4 +4,8 @@ app = FastAPI()
 
 @app.get('/')
 async def root(): return {"message": "Hello World!"}
-#uvicord main:app --reload to load the server and making it reload for each change we do
+#uvicorn main:app --reload to load the server and making it reload for each change we do
+#fastapi quickly creates a documentation of all the routes we create, and we can check then
+#through  http://127.0.0.1:8000/docs
+@app.get('/next')
+async def root2(): return {"message": "Second route"}
