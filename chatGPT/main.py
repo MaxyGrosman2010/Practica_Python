87cbec8d9d831b9e3b9a39b48e235463cba2453e
MaxyGrosman <maxygrosman@gmail.com>
2023-09-14 19:02:56 -0300

    GPT

--- chatGPT/main.py
+++ chatGPT/main.py
@@ -2,3 +2,6 @@ import openai
 import config
 
 openai.api_key = config.api_key
+response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+    messages= [{"role": "user", "content": "Hi, is it a good night in Argentina?"}])
+print(f"Message: {response.choices[0].message.content}\nRemaining usages: {response.usage}")
